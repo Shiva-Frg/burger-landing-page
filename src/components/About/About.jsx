5abc135f8e2de800f9c23bf09ebff27dc1739e6e
shiva <frg.shivaspace47@gmail.com>
2021-11-09 16:35:51 +0330

    complete About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
 
+import aboutImage from '../../assets/about.png'
+
 const About = () => {
-  return <div>About</div>
+  return (
+    <section className="mt-20">
+      <div className="flex items-center w-11/12 rounded-lg shadow-2xl p-6 m-auto space-x-7">
+        <article className="w-2/4 p-2">
+          <p className="font-bold text-2xl">DISCOVER</p>
+          <h1 className="font-black text-4xl">UPCOMING EVENTS</h1>
+          <p className="text-gray-500 mt-6 pr-12">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum.
+          </p>
+          <div className="flex items-center justify-between w-12 mt-6">
+            <labe className="text-yellow-400 font-black text-6xl">.</labe>
+            <labe className="text-gray-200 font-black text-6xl">.</labe>
+            <labe className="text-gray-200 font-black text-6xl">.</labe>
+          </div>
+        </article>
+        <div className="w-2/4">
+          <img src={aboutImage} alt="about" />
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default About
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -29,10 +29,10 @@ const Menu = () => {
     <main className="p-4 space-y-14">
       <Features />
       <section className="flex flex-col items-center justify-center space-y-5">
-        <p className="bg-yellow-400 p-2 w-52 text-center font-bold text-yellow-900">
+        <p className="bg-yellow-400 p-2 w-52 text-center font-bold">
           ALWAYS TASTY BURGER
         </p>
-        <h1 className="text-6xl text-yellow-900 font-black">CHOOSE & ENJOY</h1>
+        <h1 className="text-6xl font-black">CHOOSE & ENJOY</h1>
         <p className="text-gray-500 text-center w-4/5 font-semibold">
           Lorem ipsum is placeholder text commonly used in the graphic, print,
           and publishing industries for previewing layouts and visual mockups.
