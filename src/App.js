f5d67de611bf440b996c62465f1e3f2ca5464ff8
shiva <frg.shivaspace47@gmail.com>
2021-11-11 13:04:16 +0330

    some edit and delete react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
 
 import Layout from './container/Layout'
 
 const App = () => {
-  return (
-    <Router>
-      <Layout />
-    </Router>
-  )
+  return <Layout />
 }
 
 export default App
